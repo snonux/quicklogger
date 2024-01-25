e0610e591e250375044763ce1832f387569a3dbe
Paul Buetow <paul@buetow.org>
2024-01-26 00:08:15 +0200

    add FyneApp meta data

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [ -z "$ANDORID_NDK_HOME" ]; then
     export ANDROID_NDK_HOME=~/android-ndk/android-ndk-r26b
 fi
 
-fyne package -os android -appID org.buetow.quicklogger -icon logo-small.png 
+fyne package -os android
 
 if [ -d ~/Documents/APKs ]; then
     # Will be synced to my phone by Syncthing, so I can install it there.
